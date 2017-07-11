8738d9eb53c1d72ec32c01d58f88bfbbdd28d498
Darnell Gillead <dlgillead@gmail.com>
2017-07-11 18:43:00 -0500

    current working

---
+++ rectangles.rb
@@ -0,0 +1,44 @@
+# for both rectangles
+  # add width to leftX and add height to bottomY
+    # opposite corner will be the new X and Y
+    # bottom right corner will be new X and same Y
+    # top left corner will be new Y and same X
+  # store values for each point
+  # intersection is where point of one rectangle is contained in other rectangle
+
+my_rectangle = {
+  # coordinates of bottom-left corner
+  'left_x' => 1,
+  'bottom_y' => 5,
+
+  # width and height
+  'width' => 10,
+  'height' => 4,
+}
+
+other_rectangle = {
+  # coordinates of bottom-left corner
+  'left_x' => 6,
+  'bottom_y' => 8,
+
+  # width and height
+  'width' => 10,
+  'height' => 4,
+}
+
+def get_points(my_rectangle, other_rectangle)
+  intersection_rectangle = {}
+  if other_rectangle['left_x'] > my_rectangle['left_x'] && other_rectangle['left_x'] < my_rectangle['left_x'] + my_rectangle['width']
+    intersection_rectangle['left_x'] = other_rectangle['left_x']
+  end
+  if other_rectangle['bottom_y'] > my_rectangle['bottom_y'] && other_rectangle['bottom_y'] < my_rectangle['bottom_y'] + my_rectangle['height']
+    intersection_rectangle['bottom_y'] = other_rectangle['bottom_y']
+    intersection_rectangle['height'] = (my_rectangle['bottom_y'] + my_rectangle['height'] - intersection_rectangle['bottom_y'])
+  else
+    intersection_rectangle['bottom_y'] = my_rectangle['bottom_y']
+  end
+  intersection_rectangle['width'] = (my_rectangle['left_x'] + my_rectangle['width']) - intersection_rectangle['left_x']
+  p intersection_rectangle
+end
+
+get_points(my_rectangle, other_rectangle)
